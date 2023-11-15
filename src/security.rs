90dec588c8f85b317f335330360aad62f438170d
firasbouali11 <firas.bouali11@gmail.com>
2023-11-15 23:16:31 +0100

    seperating the private key

--- src/security.rs
+++ src/security.rs
@@ -1,5 +1,5 @@
 use sha2::{Sha512, Digest};
-use std::{fs, path};
+use std::{fs, path, env};
 use rsa::{Pkcs1v15Encrypt, PublicKey, RsaPrivateKey, RsaPublicKey};
 use rand;
 use rand::rngs::OsRng;
@@ -20,7 +20,8 @@ pub fn encrypt(password: String, user_id: i32) -> String {
 }
 
 pub fn decrypt(enc_password: String, user_id: i32) -> String {
-    let priv_key = RsaPrivateKey::read_pkcs1_pem_file(format!("keys/{}/private_key.pem", user_id)).unwrap();
+    let home_dir = env::var("HOME").expect("Error: HOME environment variable not found");
+    let priv_key = RsaPrivateKey::read_pkcs1_pem_file(format!("{}/.local/pm/keys/{}/private_key.pem", home_dir, user_id)).unwrap();
     let data = hex::decode(enc_password).unwrap();
     let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &data).expect("failed to decrypt");
     let password = String::from_utf8(dec_data.to_owned()).unwrap();
@@ -28,17 +29,20 @@ pub fn decrypt(enc_password: String, user_id: i32) -> String {
 }
 
 pub fn generateKeys(user_id: i32){
-    if !path::Path::new(format!("keys/{}", user_id).as_str()).is_dir() {
+    let home_dir = env::var("HOME").expect("Error: HOME environment variable not found");
+    if !path::Path::new(format!("{}/.local/pm/keys/{}", home_dir, user_id).as_str()).is_dir() {
         let bits = 2048;
         let priv_key = RsaPrivateKey::new(&mut OsRng, bits).expect("failed to generate a key");
         let pub_key = RsaPublicKey::from(&priv_key);
-        if !path::Path::new("keys").is_dir() {
+        if !path::Path::new(format!("{}/.local/pm/keys", home_dir).as_str()).is_dir() {
+            fs::create_dir(format!("{}/.local/pm/keys", home_dir).as_str()).expect("cannot create keys folder");
             fs::create_dir("keys").expect("cannot create keys folder");
         }
-        fs::create_dir(format!("keys/{}",user_id.to_string())).expect("cannot create user_id folder");
+        fs::create_dir(format!("{}/.local/pm/keys/{}", home_dir, user_id.to_string())).expect("cannot create user_id folder");
+        fs::create_dir(format!("keys/{}", user_id.to_string())).expect("cannot create user_id folder");
         pub_key.to_pkcs1_der().unwrap().write_pem_file(format!("keys/{}/public_key.pem",user_id.to_string()),"RSA PUBLIC KEY", LineEnding::LF)
             .expect("TODO: panic message");
-        priv_key.to_pkcs1_der().unwrap().write_pem_file(format!("keys/{}/private_key.pem",user_id.to_string()), "RSA PRIVATE KEY", LineEnding::LF)
+        priv_key.to_pkcs1_der().unwrap().write_pem_file(format!("{}/.local/pm/keys/{}/private_key.pem", home_dir, user_id.to_string()), "RSA PRIVATE KEY", LineEnding::LF)
             .expect("TODO: panic message");
     }
 }
